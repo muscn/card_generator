9fedf0da5876c21724a1a0e041e0c5ac6584597c
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-25 15:30:01 +0545

    co-ordinates in variable

--- generate.py
+++ generate.py
@@ -6,23 +6,29 @@ from PIL import ImageDraw
 # from urllib import urlretrieve
 import os
 
+# The co-ordinates
+devil_number_xy = (613, 75)
+name_xy = (433, 438)
+phone_xy = (643, 521)
+qr_xy = (65, 302)
 
+# Sample Data
 pk = 7
-full_name = u'Shashwot Adhikari'
-mobile = u'98x11x3333'
+name = u'Shashwot Adhikari'
+phone = u'98x11x3333'
 devil_number = u'#007'
 
 img = Image.open('watermarked_card.jpg')
 draw = ImageDraw.Draw(img)
 # write devil number
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.otf'), 19)
-draw.text((613, 75), devil_number, (255, 255, 255), font=font)
+draw.text(devil_number_xy, devil_number, (255, 255, 255), font=font)
 # write name
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'), 19)
-draw.text((433, 438), full_name, (255, 255, 255), font=font)
+draw.text(name_xy, name, (255, 255, 255), font=font)
 # write phone number
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'), 19)
-draw.text((643, 521), mobile, (255, 255, 255), font=font)
+draw.text(phone_xy, phone, (255, 255, 255), font=font)
 
 # download qr
 # if not os.path.exists(os.path.join(settings.MEDIA_ROOT, 'qrs')):
@@ -31,7 +37,7 @@ draw.text((643, 521), mobile, (255, 255, 255), font=font)
 #             os.path.join(settings.MEDIA_ROOT, 'qrs', str(self.id) + '.png'))
 # qr = Image.open(os.path.join(settings.MEDIA_ROOT, 'qrs', str(self.id) + '.png'))
 # write qr to image
-# draw.bitmap((65, 302), qr)
+# draw.bitmap(qr_xy, qr)
 
 if not os.path.exists('sample_cards'):
     os.makedirs('sample_cards')
