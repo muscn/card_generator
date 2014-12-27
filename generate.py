385204682dc6094188e6834ef5c101fbef2eb3d0
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-27 12:10:00 +0545

    writing of phone number from ending x

--- generate.py
+++ generate.py
@@ -9,20 +9,20 @@ import os
 # The co-ordinates
 devil_number_ending_xy = (744, 68)
 name_ending_xy = (1010, 438)
-phone_xy = (643, 521)
+phone_ending_xy = (1035, 521)
 qr_xy = (65, 302)
 
 # The font-sizes
 devil_number_size = 58
 name_size = 60
-phone_size = 19
+phone_size = 60
 
 draw_qr = False
 
 # Sample Data
 pk = 7
-name = u'Prakash Man Singh Katuwal'
-phone = u'98x11x3333'
+name = u'Prakash Man Bhakta I'
+phone = u'98x113834x'
 devil_number = u'00000007'
 
 # Pre-process the name
@@ -44,6 +44,7 @@ img = Image.open('watermarked.jpg')
 # img = Image.open('front.jpg')
 
 draw = ImageDraw.Draw(img)
+
 # write devil number
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.otf'),
                           devil_number_size)
@@ -51,28 +52,24 @@ devil_number_text_size = draw.textsize('#' + devil_number, font=font)
 devil_number_xy = (devil_number_ending_xy[0] - devil_number_text_size[0], devil_number_ending_xy[1])
 draw.text(devil_number_xy, '#' + devil_number, (255, 255, 255), font=font)
 
-
 # write name
 name_sans_last_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.otf'), name_size)
 name_sans_last_size = draw.textsize(name_sans_last, font=name_sans_last_font)
-
-
-
 last_name_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-BoldItalic.otf'), name_size)
 last_name_size = draw.textsize(last_name, last_name_font)
-
 name_length = name_sans_last_size[0] + last_name_size[0]
-# print name_length
 name_xy = (name_ending_xy[0] - name_length, name_ending_xy[1])
 last_name_xy = (name_xy[0] + name_sans_last_size[0], name_xy[1])
-draw.text(name_xy, name_sans_last, (255, 255, 255), font=font)
+draw.text(name_xy, name_sans_last, (255, 255, 255), font=name_sans_last_font)
 draw.text(last_name_xy, last_name, (255, 255, 255), font=last_name_font)
 
 
 
 # write phone number
-font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'),
+phone_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'),
                           phone_size)
+phone_size = draw.textsize(phone, phone_font)
+phone_xy = (phone_ending_xy[0] - phone_size[0], phone_ending_xy[1])
 draw.text(phone_xy, phone, (255, 255, 255), font=font)
 
 
