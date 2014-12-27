a21bac7c3a366b81ad0f6bbe14d74f8dfd8b2645
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-27 09:28:50 +0545

    fixed devil number co-ordinates and size

--- generate.py
+++ generate.py
@@ -7,15 +7,17 @@ from urllib import urlretrieve
 import os
 
 # The co-ordinates
-devil_number_xy = (613, 75)
+devil_number_xy = (614, 68)
 name_xy = (433, 438)
 phone_xy = (643, 521)
 qr_xy = (65, 302)
 
 # The font-sizes
-devil_number_size = 19
-name_size = 19
+devil_number_size = 58
+name_size = 60
 phone_size = 19
+
+draw_qr = False
 
 # Sample Data
 pk = 7
@@ -23,13 +25,16 @@ name = u'Shashwot Adhikari'
 phone = u'98x11x3333'
 devil_number = u'007'
 
+
 img = Image.open('watermarked.jpg')
 # img = Image.open('watermarked_with_qr.jpg')
+# img = Image.open('front.jpg')
+
 draw = ImageDraw.Draw(img)
 # write devil number
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.otf'),
                           devil_number_size)
-draw.text(devil_number_xy, devil_number, (255, 255, 255), font=font)
+draw.text(devil_number_xy, '#' + devil_number, (255, 255, 255), font=font)
 # write name
 font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'),
                           name_size)
@@ -39,27 +44,29 @@ font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'),
                           phone_size)
 draw.text(phone_xy, phone, (255, 255, 255), font=font)
 
-# download qr
-if not os.path.exists('qrs'):
-    os.makedirs('qrs')
-urlretrieve(
-            'http://api.qrserver.com/v1/create-qr-code/?data=http://manutd.org.np/' + devil_number + '&size=160x160&ecc=H&color=ffffff&bgcolor=000',
-            os.path.join('qrs', str(pk) + '.png'))
-qr = Image.open(os.path.join('qrs', str(pk) + '.png'))
-#make qr transparent
-qr = qr.convert('RGBA')
-data = qr.getdata()
-new_data = []
-for item in data:
-    if item[0] == 0 and item[1] == 0 and item[2] == 0:
-        new_data.append((255, 255, 255, 0))
-    else:
-        new_data.append(item)
-qr.putdata(new_data)
-qr.save(os.path.join('qrs', str(pk) + '.png'))
-# write qr to image
-img = img.convert('RGBA')
-img.paste(qr, qr_xy, qr)
+
+if draw_qr:
+    # download qr
+    if not os.path.exists('qrs'):
+        os.makedirs('qrs')
+    urlretrieve(
+                'http://api.qrserver.com/v1/create-qr-code/?data=http://manutd.org.np/' + devil_number + '&size=160x160&ecc=H&color=ffffff&bgcolor=000',
+                os.path.join('qrs', str(pk) + '.png'))
+    qr = Image.open(os.path.join('qrs', str(pk) + '.png'))
+    #make qr transparent
+    qr = qr.convert('RGBA')
+    data = qr.getdata()
+    new_data = []
+    for item in data:
+        if item[0] == 0 and item[1] == 0 and item[2] == 0:
+            new_data.append((255, 255, 255, 0))
+        else:
+            new_data.append(item)
+    qr.putdata(new_data)
+    qr.save(os.path.join('qrs', str(pk) + '.png'))
+    # write qr to image
+    img = img.convert('RGBA')
+    img.paste(qr, qr_xy, qr)
 
 if not os.path.exists('sample_cards'):
     os.makedirs('sample_cards')
