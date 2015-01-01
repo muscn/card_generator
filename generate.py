a5e009ae7137c61914c8adcc7148751dfdc7a002
Dipesh Acharya <xtranophilist@gmail.com>
2015-01-01 15:30:43 +0545

    fixed phone style

--- generate.py
+++ generate.py
@@ -10,13 +10,13 @@ import re
 # The co-ordinates
 devil_number_ending_xy = (744, 68)
 name_ending_xy = (1010, 438)
-phone_ending_xy = (1035, 521)
+phone_ending_xy = (1010, 520)
 qr_xy = (65, 302)
 
 # The font-sizes
 devil_number_size = 58
 name_size = 60
-phone_size = 60
+phone_size = 43
 
 draw_qr = False
 
@@ -47,6 +47,7 @@ if matches:
 
     phone_code = matches.groups()[0]
     phone = matches.groups()[1]
+    phone_code = '+' + phone_code + '  '
 else:
     phone_code = ''
 
@@ -67,7 +68,7 @@ draw.text(devil_number_xy, '#' + devil_number, (255, 255, 255), font=devil_numbe
 # write name
 name_sans_last_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.otf'), name_size)
 name_sans_last_size = draw.textsize(name_sans_last, font=name_sans_last_font)
-last_name_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-BoldItalic.otf'), name_size)
+last_name_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Italic.otf'), name_size)
 last_name_size = draw.textsize(last_name, last_name_font)
 name_length = name_sans_last_size[0] + last_name_size[0]
 name_xy = (name_ending_xy[0] - name_length, name_ending_xy[1])
@@ -78,7 +79,7 @@ draw.text(last_name_xy, last_name, (255, 255, 255), font=last_name_font)
 
 
 # write phone number
-phone_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Regular.otf'),
+phone_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-Italic.otf'),
                                      phone_size)
 phone_font_size = draw.textsize(phone, phone_font)
 phone_xy = (phone_ending_xy[0] - phone_font_size[0], phone_ending_xy[1])
@@ -87,7 +88,6 @@ phone_code_font = ImageFont.truetype(os.path.join('fonts', 'Aileron-ThinItalic.o
                                      phone_size)
 phone_code_size = draw.textsize(phone_code, phone_code_font)
 phone_code_xy = (phone_xy[0] - phone_code_size[0], phone_ending_xy[1])
-print phone_code == ''
 draw.text(phone_code_xy, phone_code, (255, 255, 255), font=phone_code_font)
 
 
